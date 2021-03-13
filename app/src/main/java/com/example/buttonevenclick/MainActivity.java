d99459b990fa23c9135cfa744c2a4515bba4060b
GreatD <devamahendrar@gmail.com>
2021-03-13 15:37:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/buttonevenclick/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.buttonevenclick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnsignin;
+    EditText edpassword, edemail;
+    String nama, password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnsignin= findViewById(R.id.btnSigIn);
+        edemail = findViewById(R.id.edEmail);
+        edpassword = findViewById(R.id.edPassword);
+
+        btnsignin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                nama = edemail.getText().toString();
+                password = edpassword.getText().toString();
+
+
+
+                Toast t = Toast.makeText(getApplicationContext(),
+                        "email anda: "+nama+" dan password anda: "+password+"", Toast.LENGTH_LONG);
+                t.show();
+
+
+            }
+        });
+    }
+
+}
